46ade14b727e9d4370695b0aa127e41cb35ec02b
MateoCata98 <mate.catalano98@gmail.com>
2022-08-29 23:58:13 -0300

    make it better

--- src/main/java/com/portfolio/mateocatalano/Controller/ExperienciaController.java
+++ src/main/java/com/portfolio/mateocatalano/Controller/ExperienciaController.java
@@ -10,7 +10,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/experiencia")
-
+@CrossOrigin(origins = "https://mateocatalano-portfolio.web.app")
 public class ExperienciaController {
    
     @Autowired
