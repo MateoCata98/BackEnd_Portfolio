e89a37f68a83bfea91be5ed6e1f7bafbab60abdb
Mateo Catalano <100876278+MateoCata98@users.noreply.github.com>
2022-11-14 19:25:59 -0300

    Update AuthController.java

--- src/main/java/com/portfolio/mateocatalano/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/mateocatalano/Security/Controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://mateojuancatalano.web.app")
+@CrossOrigin(origins = "https://mateo-catalano.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
